8d81f3a4616d61287506efff356a18cc62e220b5
williamjones62 <williamjones62@gmail.com>
2017-07-31 19:44:02 -0400

    add game and face classes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 import './styles.css';
 
+class Face extends Component {
+
+    constructor() {
+      super();
+      this.state = {
+        clicked: false
+      };
+    }
+
+    onClick() {
+          this.setState({clicked: true});
+    }
+
+    render() {
+        let classes = React.addons.classSet({
+            'name': true,
+            'hidden': !this.state.clicked,
+            'right': this.state.clicked && this.props.selected,
+            'wrong': this.state.clicked && !this.props.selected
+        });
+
+        return (
+            <article className="employee" onClick={this.onClick}>
+                <img src={ this.props.url } />
+                <span className={classes}>
+                    <h2>{this.props.name}</h2>
+                </span>
+            </article>
+        );
+    }
+};
+
+class Game extends Component {
+
+    render() {
+      var faces = this.props.people.map(function(p) {
+          return <Face key={Math.random()} url={'http:' + p.headshot.url} name={p.firstName + ' ' + p.lastName} selected={p.selected}/>;
+      });
+
+      var selectedFace = this.props.people.find(function (o) { return o.selected; });
+      return (
+        <div>
+          <h1 id="question-wrapper">Who is {selectedFace.firstName + ' ' + selectedFace.lastName}?</h1>
+          <div>{faces}</div>
+        </div>
+      );
+    }
+  }
+
 class App extends Component {
   constructor() {
     super();
@@ -19,6 +68,10 @@ class App extends Component {
   }
 
   render() {
+    let game;
+    if (this.state.people.length) {
+        game = <Game people={this.state.people} />;
+    }
     return (
       <div>
           <button> Play </button>
