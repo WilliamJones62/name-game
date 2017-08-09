16b8f277a818faa70a239a4fc071663471f2b4f8
williamjones62 <williamjones62@gmail.com>
2017-08-09 09:30:38 -0400

    Restart

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+fetch('https://willowtreeapps.com/api/v1.0/profiles/')
+  .then(response => response.json())
+  .catch(e => console.log('error', e));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
