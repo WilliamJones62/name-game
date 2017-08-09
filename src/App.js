9ac066285cf2e18d34563fb8c742406317d6922b
williamjones62 <williamjones62@gmail.com>
2017-08-09 17:41:35 -0400

    Move fetch to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 import './styles.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [],
+      myPeeps: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://willowtreeapps.com/api/v1.0/profiles/')
+      .then(response => response.json())
+      .then(data => this.setState({ people: data.items }))
+      .catch(e => console.log('error', e));
+  };
+
   render() {
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-fetch('https://willowtreeapps.com/api/v1.0/profiles/')
-  .then(response => response.json())
-  .catch(e => console.log('error', e));
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
